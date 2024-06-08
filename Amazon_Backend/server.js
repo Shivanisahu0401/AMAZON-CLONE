00daecf6de3bb04ca39c0e38cef24b60cc70f6d9
Shivani Sahu <130729441+Shivanisahu0401@users.noreply.github.com>
2024-06-08 21:54:23 +0530

    Update server.js

--- Amazon_Backend/server.js
+++ Amazon_Backend/server.js
@@ -4,8 +4,8 @@ const app = require('./app.js')
 const addDataToServer = require('./dataBackup/addDataToServer.js')
 
 const url = 'mongodb+srv://_USERNAME_:_PASSWORD_@cluster0.3pqyhsm.mongodb.net/_DATABASENAME_?retryWrites=true&w=majority&appName=Cluster0'
-const dataBaseUser = 'SparshChauhan'
-const dataBasePassword = 'chauhansparsh112'
+const dataBaseUser = 'root'
+const dataBasePassword = 'Vaishnavi@2001'
 const dataBaseName = 'Amazon-Backend'
 
 let dbLink = url.replace("_USERNAME_",dataBaseUser)
